1db22fead13ff140937ccdf0724c0e33bd7f8813
codingJWilliams <course@jaywilliams.me>
2015-11-04 19:59:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #select file location
 flocation = 'words.txt'
+# start code
+
+
+
+
+
 thelist = [line.rstrip('\n') for line in open(flocation)]
 print("Scrabble helper.")
 letters = str(input("Please enter the letters you have to work with.\n>"))
