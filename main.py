a6b395c9274ecff9f4f8f62ccf0b3bb104aa46db
Jay W <coding@jaywilliams.me>
2015-11-04 19:57:46 +0000

    Mini-commit

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ flocation = 'words.txt'
 thelist = [line.rstrip('\n') for line in open(flocation)]
 print("Scrabble helper.")
 letters = str(input("Please enter the letters you have to work with.\n>"))
+wordsdone = 0
+while wordsdone == len(thelist):
 
